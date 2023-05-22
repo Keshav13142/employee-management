df9c3008c2153bbe3cca8a4d0310c248bea5b0a0
keshav13142 <s.keshav13142@gmail.com>
2023-05-22 18:02:15 +0530

    refactor: move api calls to seperate files

---
+++ src/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
+export const getAllEmployees = async () => {
+  const { data } = await client.get("/employees");
+  return data;
+};
+
+export const getAllDepts = async () => {
+  const { data } = await client.get("/department");
+  return data;
+};
+
+export const getAllRoles = async () => {
+  const { data } = await client.get("/role");
+  return data;
+};
