41f411098b798de74ad62a4c911a278858d03b58
keshav13142 <s.keshav13142@gmail.com>
2023-05-23 16:14:42 +0530

    feat: moved frontend to a separate folder

--- frontend/src/components/AddEmp.jsx
+++ frontend/src/components/AddEmp.jsx
@@ -117,7 +117,7 @@ const DialogDemo = () => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-blackA9 data-[state=open]:animate-overlayShow" />
-        <Dialog.Content className="fixed left-[50%] top-[50%] max-h-[95vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
+        <Dialog.Content className="fixed left-[50%] top-[50%] max-h-[95vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] overflow-y-auto rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
           <Dialog.Title className="m-0 mb-5 mt-[10px] text-[17px] font-medium text-mauve12">
             Add a new Employee
           </Dialog.Title>
