29447fc97e45fb755a47ce6cdddb93a40c90de87
keshav13142 <s.keshav13142@gmail.com>
2023-05-26 20:29:16 +0530

    feat: better redirection

--- frontend/src/components/ContextProvider.jsx
+++ frontend/src/components/ContextProvider.jsx
@@ -1,14 +1,27 @@
 import { createContext, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   useEffect(() => {
     if (!user) {
       const local_user = JSON.parse(localStorage.getItem("user"));
-      if (local_user) setUser(local_user);
+      if (local_user) {
+        setUser(local_user);
+
+        if (pathname === "/") return;
+
+        local_user.type === "ADMIN"
+          ? navigate("/admin")
+          : navigate("/employee");
+      } else {
+        navigate("/");
+      }
     } else {
       localStorage.setItem("user", JSON.stringify(user));
     }
